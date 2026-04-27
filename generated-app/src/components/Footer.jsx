9248bac87d9c555823b1da945f97bca8828f15ff
Sherif Rizk <rizksn@gmail.com>
2026-04-27 14:26:17 -0400

    Add app deletion flow

--- generated-app/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-800 p-4 mt-6">
-      <p className="text-center text-gray-400">© 2023 My Landing Page. All rights reserved.</p>
-    </footer>
-  );
-}
